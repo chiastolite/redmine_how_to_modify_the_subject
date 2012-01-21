05cfeb7a4c95594f270d4823b7ee02ee1a396fb9
Hiroyuki MORITA <chiastolite.1980@gmail.com>
2012-01-22 00:15:19 +0900

    create plugin

---
+++ README.markdown
@@ -0,0 +1,4 @@
+Redmine How To Modify the Subject? plugin
+===
+
+This plugin only changes a 'more' label.
---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_how_to_modify_the_subject do
+  name 'Redmine How To Modify the Subject? plugin'
+  author 'Hiroyuki MORITA'
+  version '0.0.1'
+  url 'https://github.com/chiastolite/redmine_how_to_modify_the_subject'
+end
